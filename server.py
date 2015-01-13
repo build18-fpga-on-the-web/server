23e93bc14747940331fd2fec96cd9b4ac5501b05
rmaratos <rmaratos@andrew.cmu.edu>
2015-01-12 21:56:55 -0500

    Input from switches and keys

--- server.py
+++ server.py
@@ -13,12 +13,12 @@ num = {"ledr":18, "ledg":9, "hex":8, "segments":7, "sw":18, "key":4}
 
 def init_outputs():
     outputs["ledr"] = [0]*18
-    outputs["ledg"] = [0]*9
-    outputs["hex"] = [[0]*7 for _ in range(8)]
+    outputs["ledg"] = [1]*9
+    outputs["hex"] = [[1]*7 for _ in range(8)]
 
 def init_inputs():
-    inputs["sw"] = [0]*18
-    inputs["key"] = [0]*4
+    inputs["sw"] = [1]*18
+    inputs["key"] = [1]*4
 
 class IndexHandler(RequestHandler):
     @asynchronous
@@ -30,11 +30,23 @@ class WebSocketChatHandler(WebSocketHandler):
     def open(self, *args):
         print("open", "WebSocketChatHandler")
         clients.append(self)
+        data = {"inputs":inputs, "outputs":outputs}
         self.write_message(json_encode(data))
 
     def on_message(self, message):
         print("Message from client")
         print(message)
+        group = None
+        if message.startswith("sw"):
+            group = "sw"
+        if message.startswith("key"):
+            group = "key"
+        if group:
+            try:
+                i = int(message[len(group):])
+            except ValueError:
+                print("int failed on", message)
+            inputs[group][i] = 1 - inputs[group][i]
         data = {"inputs":inputs, "outputs":outputs}
         for client in clients:
             client.write_message(json_encode(data))
--- static/board.js
+++ static/board.js
@@ -1,11 +1,60 @@
 /*
  * board.js - manage board
  */
+
+function update_fill(data, class_name, color) {
+  for (var i=0; i<data.length; i++)
+  {
+    fill = (data[i]) ? color : "white";
+    id = class_name + i;
+    $(id).css("background-color", fill);
+  }
+}
+
+function update_fills(data) {
+  var outputs = data["outputs"];
+  var inputs = data["inputs"];
+  update_fill(outputs["ledr"], "#ledr", "red");
+  update_fill(outputs["ledg"], "#ledg", "green");
+  var hex = outputs["hex"];
+  for (var i=0; i<hex.length; i++)
+  {
+    update_fill(hex[i], "#hex"+i+"-", "red");
+  }
+  update_fill(inputs["sw"], "#sw", "grey");
+  update_fill(inputs["key"], "#key", "grey");
+}
+
 var board = {
   init: function() {
     console.log("Initializing board");
-    init_ws(WS_URL);
-  }
+    this.init_ws(WS_URL);
+    $(".sw").click(function() {
+      ws.send(this.id);
+    });
+    $(".key").click(function() {
+      ws.send(this.id);
+    });
+  },
+
+  init_ws: function(url) {
+    console.log("Opening WebSocket");
+    console.log(url);
+    ws = new WebSocket(url);
+    ws.onmessage = function(e) {
+      console.log("new message");
+      console.log(e);
+      console.log(typeof(e.data));
+      console.log(e.data);
+      var data = JSON.parse(e.data);
+      update_fills(data);
+  };
+
+  ws.onclose = function(e) {
+    console.log("WebSocket Closed");
+    init_ws(url);
+  };
+}
 };
 
 // var box = $("#box");
