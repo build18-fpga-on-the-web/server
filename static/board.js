80f6ea1c16bf000cde213aa4e6c50547012e009f
rmaratos <rmaratos@andrew.cmu.edu>
2015-01-13 09:34:36 -0500

    remove dead js

--- static/board.js
+++ static/board.js
@@ -57,23 +57,3 @@ var board = {
   };
 }
 };
-
-// var box = $("#box");
-//   $("[id^=box]").click(function() {
-//       console.log("clicked box!");
-//       console.log(this.id);
-//       ws.send(this.id.slice(3));
-//     });
-
-//   function update_fill(data){
-//     console.log("updating fill");
-//     for (var i=0; i<10; i++)
-//     {
-//       fill = (data[i]) ? "green" : "white";
-//       console.log(data[i]);
-//       console.log(fill);
-//       id = "#box" + i;
-//       console.log(id);
-//       $(id).css("background-color", fill);
-//     }
-//   }
